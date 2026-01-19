c6d0068e232b21508b69c6109f75b248d9a729a3
MPAnilK <MPAnilK@U-H8PCP13.lan>
2026-01-19 11:16:02 +0530

    Firstcommit

---
+++ Selenium_Ani/src/test/java/Testingexamples/Findelements.java
@@ -0,0 +1,30 @@
+package Testingexamples;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Findelements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://financialservices.gov.in/beta/en");
+		List<WebElement> link = driver.findElements(By.tagName("a"));
+		System.out.println(link.size());
+		for(int i=0;i<=link.size();i++)
+		{
+		WebElement links=link.get(i);
+		System.out.println(links.getText());
+		System.out.println(links.getAttribute("href"));
+		System.out.println(links);
+		
+		}
+	}
+
+}
