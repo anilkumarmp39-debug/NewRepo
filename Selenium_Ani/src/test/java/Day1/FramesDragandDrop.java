dbdf22b106f7b2b1247da47a5ffcb55994415d29
MPAnilK <MPAnilK@U-H8PCP13.lan>
2026-01-20 11:28:36 +0530

    DragandDropcommit

---
+++ Selenium_Ani/src/test/java/Day1/FramesDragandDrop.java
@@ -0,0 +1,31 @@
+package Day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.reactivex.rxjava3.functions.Action;
+
+public class FramesDragandDrop {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://jqueryui.com/droppable/");
+		Thread.sleep(2000);
+		WebElement ifra = driver.findElement(By.xpath("//iframe[@class='demo-frame']"));
+		driver.switchTo().frame(ifra);
+		WebElement drag= driver.findElement(By.id("draggable"));
+		WebElement drop=driver.findElement(By.id("droppable"));
+		Actions action=new Actions(driver);
+		action.dragAndDrop(drag, drop).perform();
+		
+		
+	}
+
+}
